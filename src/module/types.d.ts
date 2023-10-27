6d9ef91dc30782782f680ca574ce4a7d13bfe684
OmegaRogue <omegarogue@omegavoid.codes>
2023-10-27 17:32:37 +0200

    :rotating_light: Fix linter errors

--- src/module/types.d.ts
+++ src/module/types.d.ts
@@ -1,10 +1,11 @@
 import { id as MODULE_ID } from "../../static/module.json";
 
-
 declare global {
 	interface ClientSettings {
 		get(module: MODULE_ID, setting: "mobile-mode"): "on" | "off" | "auto";
+
 		get(module: MODULE_ID, setting: "send-button"): "on" | "off" | "auto";
+
 		get(module: MODULE_ID, setting: "close-button-text"): "on" | "off" | "auto";
 	}
 }
